3eb5bc5ec62dc510952a07cdbac4ed6e1a9e436f
lera <lera_bal@list.ru>
2023-02-15 13:21:51 +0300

    function_AddArray

--- final_project/Program.cs
+++ final_project/Program.cs
@@ -6,8 +6,20 @@
     int result = int.Parse(Console.ReadLine()!);
     return result;
 }
+string[] AddArray(int size)
+{
+    string[] arr1 = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        string result = Console.ReadLine()!;
+        arr1[i] = result;
+    }
+    return arr1;
+}
 
 int size = GetUserData("Введите количество элементов массива: ");
+string[] array = AddArray(size);
 
 
 
